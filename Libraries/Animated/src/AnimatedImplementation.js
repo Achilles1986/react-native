Achilles1986/react-native

commit c9960817eea008b2703657066212002557e0d939

Add viewConfig to RCTSlider Component, fix #10158
Summary:
And check componet.viewConfig before call component.setNativeProps method.

FIX #10158
Closes https://github.com/facebook/react-native/pull/10238

Differential Revision: D4008640

Pulled By: javache

fbshipit-source-id: 1bf9b34c0e56f1ffbcd31ae64b3cbafe77a9a1fd

Libraries/Animated/src/AnimatedImplementation.js
@@ -1715,6 +1715,12 @@ function createAnimatedComponent(Component: any): any {
       var callback = () => {
         if (this._component.setNativeProps) {
           if (!this._propsAnimated.__isNative) {
+            if (this._component.viewConfig == null) {
+              var ctor = this._component.constructor;
+              var componentName = ctor.displayName || ctor.name || '<Unknown Component>';
+              throw new Error(componentName + ' "viewConfig" is not defined.');
+            }
+
             this._component.setNativeProps(
               this._propsAnimated.__getAnimatedValue()
             );

Examples/UIExplorer/js/RTLExample.js
@@ -47,8 +47,6 @@ const {
 const UIExplorerPage = require('./UIExplorerPage');
 const UIExplorerBlock = require('./UIExplorerBlock');
 
-const AnimatedImage = Animated.createAnimatedComponent(Image);
-
 type State = {
   toggleStatus: any,
   pan: Object,
@@ -116,7 +114,7 @@ function AnimationBlock(props) {
   return (
     <View style={styles.block}>
       <TouchableWithoutFeedback onPress={props.onPress}>
-        <AnimatedImage
+        <Animated.Image
           style={[styles.img, props.imgStyle]}
           source={require('./Thumbnails/poke.png')}
         />

Libraries/Components/Slider/Slider.js
@@ -13,6 +13,7 @@
 
 var Image = require('Image');
 var NativeMethodsMixin = require('react/lib/NativeMethodsMixin');
+var ReactNativeViewAttributes = require('ReactNativeViewAttributes');
 var Platform = require('Platform');
 var PropTypes = require('react/lib/ReactPropTypes');
 var React = require('React');
@@ -137,6 +138,14 @@ var Slider = React.createClass({
       maximumValue: 1,
       step: 0
     };
+  },
+
+  viewConfig: {
+    uiViewClassName: 'RCTSlider',
+    validAttributes: {
+      ...ReactNativeViewAttributes.RCTView,
+      value: true
+    }
   },
 
   render: function() {

commit 1d634c82b296676fc17a57bbb197ead1e8b0ad1c

Unbreak custom animated components
Summary:
Commit https://github.com/facebook/react-native/commit/c9960817eea008b2703657066212002557e0d939 broke `Animated.createAnimatedComponent()` for most components outside of the standard ones. This is because it will throw an exception for any component without a `viewConfig` defined, which basically no composition style component has AFAIK.

Related issues: https://github.com/facebook/react-native/issues/10825, https://github.com/oblador/react-native-vector-icons/issues/337, https://github.com/oblador/react-native-animatable/issues/70

This PR removes the check which is more treating the symptoms really – but shouldn't `setNativeProps` duck typing be enough?
Closes https://github.com/facebook/react-native/pull/10827

Differential Revision: D4178183

Pulled By: ericvicenti

fbshipit-source-id: b85cc78ed6d84dada4d476caa243332eaadb003f

Libraries/Animated/src/AnimatedImplementation.js
@@ -1750,12 +1750,6 @@ function createAnimatedComponent(Component: any): any {
       var callback = () => {
         if (this._component.setNativeProps) {
           if (!this._propsAnimated.__isNative) {
-            if (this._component.viewConfig == null) {
-              var ctor = this._component.constructor;
-              var componentName = ctor.displayName || ctor.name || '<Unknown Component>';
-              throw new Error(componentName + ' "viewConfig" is not defined.');
-            }
-
             this._component.setNativeProps(
               this._propsAnimated.__getAnimatedValue()
             );

Libraries/Renderer/src/renderers/native/NativeMethodsMixin.js
@@ -139,6 +139,12 @@ var NativeMethodsMixin = {
    * Manipulation](docs/direct-manipulation.html)).
    */
   setNativeProps: function(nativeProps: Object) {
+    if (!this.viewConfig) {
+      var ctor = this.constructor;
+      var componentName = ctor.displayName || ctor.name || '<Unknown Component>';
+      invariant(false, componentName + ' "viewConfig" is not defined.');
+    }
+
     if (__DEV__) {
       warnForStyleProps(nativeProps, this.viewConfig.validAttributes);
     }